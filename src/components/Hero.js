edd2db47d5aad06ef22744f65c7f251f31630180
Praise Imafidon <108192872+PraiseImafidon@users.noreply.github.com>
2022-11-27 14:39:47 +0100

    create hero section in progress

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+export default function HeroSection() {
+    return (
+        <div className="hero">
+          <div className="container">
+            <h1 className="hero__heading">
+              <span>Order food anytime, anywhere</span>
+            </h1>
+          </div>
+        </div>
+    );
+  }
